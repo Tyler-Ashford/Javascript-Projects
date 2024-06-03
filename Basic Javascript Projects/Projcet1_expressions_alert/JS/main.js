1a9868eefba33eaf6bec321d541a79b8c927aa8a
Tyler-Ashford <tyashford30@gmail.com>
2024-06-03 17:49:39 -0400

    Updated main.js file!

---
+++ Basic Javascript Projects/Projcet1_expressions_alert/JS/main.js
@@ -0,0 +1,40 @@
+// alert("Hello, World!");
+
+// window.alert("Hello, World!");
+
+// document.write("Hello, World!");
+
+// var A = "My name is Tyler!";
+//   document.write(A);
+
+// var toNice = "Hi, I am too Nice!";
+//   window.alert(toNice);
+
+// document.write("Jose told Tyler, \"Don't past me Loser!. Or you are gone!");
+
+// document.write("\"Be who you are and say what you feel," + " because those who mind don\t matter and those who matter don\t mind.\"" + "-Dr. Seuss");
+
+// var toLive = "Here we" + " Go!";
+//   document.write(toLive);
+
+// var Family = "The Ashford's", Dad = "Tyler", Mom = "Ashley", Daughter = "Tiana", Son = "Tyrie";
+  // document.write(Son);
+
+  // var sum = 3 + 3;
+  //   document.write(sum);
+
+  // function My_First_Function() {   //Defining a function and naming it
+  //   var str = "This is the button text!";  //Defining a variable and giving it a string.
+
+  //   document.getElementById("Button_Text").innerHTML = str; // Putting the value of the variable into the HTML element From Point with "Button_Text" id.
+  // }
+
+  var sent1 = "This is the beginning of the string"; //Assigning variable sent1 a value of a string of "This is the beginning of the string"
+  var sent2 = "and this is the end of the string"; //Assigning variable sent2 a value of a string "and this is the end of the string"
+
+    window.alert(`${sent1}, ${sent2}`);
+
+    6 + 6;
+
+    document.write(6 + 6);
+
