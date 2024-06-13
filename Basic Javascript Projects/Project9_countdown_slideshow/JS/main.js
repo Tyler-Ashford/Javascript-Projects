5f6a47b14000063ed05ebcb7c4ecbc215f62dab1
Tyler-Ashford <tyashford30@gmail.com>
2024-06-13 12:50:16 -0400

    Updated project 9 project folder

---
+++ Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,30 @@
+function countdown() {
+  var seconds = document.getElementById("seconds").value;
+
+  function tick() {
+    seconds = seconds - 1;
+    timer.innerHTML - seconds;
+    var time = setTimeout(tick, 1000);
+    if (seconds == -1) {
+      alert("Times's up!");
+      clearTimeout(time);
+      timer.innerHTML = "";
+    }
+  }
+  tick();
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
